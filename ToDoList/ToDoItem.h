e6259c15e72f0bd57732f5a25feb8ba742e19aa3
Lucas <lucas@agtlucas.com>
2015-01-24 08:55:20 -0200

    Adding ToDoItem Class

---
+++ ToDoList/ToDoItem.h
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.h
+//  ToDoList
+//
+//  Created by Lucas on 1/24/15.
+//  Copyright (c) 2015 AWLKA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToDoItem : NSObject
+
+@end
---
+++ ToDoList/ToDoItem.m
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.m
+//  ToDoList
+//
+//  Created by Lucas on 1/24/15.
+//  Copyright (c) 2015 AWLKA. All rights reserved.
+//
+
+#import "ToDoItem.h"
+
+@implementation ToDoItem
+
+@end
