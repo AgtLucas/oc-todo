100d3a4175f99e81f912a654cda77e85dbc337a3
Lucas <lucas@agtlucas.com>
2015-01-24 11:18:45 -0200

    Updates

--- ToDoList/ToDoListTableViewController.m
+++ ToDoList/ToDoListTableViewController.m
@@ -62,6 +62,12 @@
     
     ToDoItem *toDoItem = [self.toDoItems objectAtIndex:indexPath.row];
     cell.textLabel.text = toDoItem.itemName;
+
+    if (toDoItem.completed) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
     
     return cell;
 }
